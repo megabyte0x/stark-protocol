b712e5f3664c007f0a2eff36926a2c0c279bc79c
Yash Garg <yashygkf@gmail.com>
2022-07-10 15:57:52 +0530

    corrected: minute error like wrong naming, wrong wrapping, etc.

--- contracts/Lend.sol
+++ contracts/Lend.sol
@@ -12,7 +12,7 @@ contract Lend is Context {
         uint256 totalAmount; // * Total amount borrowed by the borrower
         uint256 amountPaidTotal; // * Amount paid by the borrower in total
         uint256 instalmentAmt; // * Amount to be paid per insalment
-        uint64 timeRentedSince; // * Time when the deal started
+        uint256 timeRentedSince; // * Time when the deal started
         uint8 noOfInstalments; // * No of instalments in which borrower will pay amount
         uint8 interestRate; // * Interest rate decided by the lender.
         bool addedInstalments; // * If borrower got more instalments after request.
@@ -24,19 +24,24 @@ contract Lend is Context {
         address _borrower,
         address _lender,
         uint256 _instalmentAmount,
-        uint256 _totalAmoount,
+        uint256 _totalAmount,
         uint8 _noOfInstalments
     ) {
         deployer = _msgSender();
         borrower = _borrower;
         lender = _lender;
 
-        deal = DealDetials({
-            instalmentAmount: _instalmentAmount,
-            noOfInstalment: _noOfInstalments,
-            totalAmount: _totalAmoount
-        });
-        deal.timeRentedSince = block.timestamp;
+        DealDetials storage dealDetails = deal;
+
+        // deal = DealDetials({
+        //     instalmentAmount: _instalmentAmount,
+        //     noOfInstalment: _noOfInstalments,
+        //     totalAmount: _totalAmoount
+        // });
+        dealDetails.instalmentAmt = _instalmentAmount;
+        dealDetails.noOfInstalments = _noOfInstalments;
+        dealDetails.totalAmount = _totalAmount;
+        deal.timeRentedSince = uint256(block.timestamp);
     }
 
     modifier onlyBorrower() {
@@ -49,7 +54,7 @@ contract Lend is Context {
         _;
     }
 
-    function payAtOnce() external onlyBorrower {
+    function payAtOnce() external payable onlyBorrower {
         DealDetials storage dealDetails = deal;
         require(dealDetails.noOfInstalments > 0, "ERR:NM"); // NM => No more installments
 
@@ -62,7 +67,7 @@ contract Lend is Context {
         deal.amountPaidTotal += value;
     }
 
-    function payInInstallment() external onlyBorrower {
+    function payInInstallment() external payable onlyBorrower {
         DealDetials storage dealDetails = deal;
 
         require(dealDetails.noOfInstalments <= 0, "ERR:NM"); // NM => No more installments
@@ -74,18 +79,18 @@ contract Lend is Context {
 
         if (dealDetails.addedInstalments) {
             uint256 amtToLeder = dealDetails.instalmentAmt +
-                (interest * 95 * 10**16);
+                (interestAmt * 95 * 10**16);
             uint256 amtToProtocol = interestAmt * 5 * 10**16;
 
             (bool successInLender, ) = lender.call{value: amtToLeder}("");
-            require(success, "ERR:OT"); //OT => On Transfer
+            require(successInLender, "ERR:OT"); //OT => On Transfer
 
             (bool successInBorrower, ) = deployer.call{value: amtToProtocol}(
                 ""
             );
-            require(success, "ERR:OT"); //OT => On Transfer
+            require(successInBorrower, "ERR:OT"); //OT => On Transfer
         } else {
-            uint256 amtToLenderOnly = dealDetails.instalmentAmt + interest;
+            uint256 amtToLenderOnly = dealDetails.instalmentAmt + interestAmt;
 
             (bool success, ) = lender.call{value: amtToLenderOnly}("");
             require(success, "ERR:OT"); //OT => On Transfer
