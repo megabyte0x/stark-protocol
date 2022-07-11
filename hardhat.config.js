12cb084f8d925b7b6693352fef3360f67b2bda35
Yash Garg <yashygkf@gmail.com>
2022-07-12 00:21:17 +0530

    edit: configired deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const POLYGON_PRIVATE_KEY = process.env.POLYGON_PRIVATE_KEY;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.15",
+  networks: {
+    polygon: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [POLYGON_PRIVATE_KEY]
+    }
+  }
 };
