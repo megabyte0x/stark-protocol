bc49cc8a6b8e59034a989f8451b8d75a7de1cde8
Yash Garg <yashygkf@gmail.com>
2022-07-17 23:23:39 +0530

    delete: duplicate contracts moved: Deployer.sol from p2p to root contract folder

--- backend/contracts/Deployer.sol
+++ backend/contracts/Deployer.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/utils/Context.sol";
-import "./Deal.sol";
+import "./p2p/Deal.sol";
 
 contract deployer_contract is Context {
     deal_contract private dealContract;
@@ -13,7 +13,7 @@ contract deployer_contract is Context {
         owner = _msgSender();
     }
 
-    struct Request {
+    struct p2pRequest {
         address borrower; // * Address of the borrower
         address lender; // * Address of the Lender
         address dealAddress; // * Address of the Deal Contract
@@ -21,25 +21,39 @@ contract deployer_contract is Context {
         uint256 totalAmount; // * Total Amount borrowed
         uint256 interestRate; // * Interest Rate by the Lender
         uint16 noOfInstalments; // * No of Instalments
-        bool requestAccepted; // * Request Raised by the lender or not
+        bool requestAccepted; // * Request Raised by the lender accepted or not
     }
 
-    Request private request;
+    p2pRequest private p2pRequestInstance;
 
-    // * To store all the requests made in the protocol
-    mapping(address => Request) private requests;
+    // * To store all the p2pRequests made in the protocol
+    mapping(address => p2pRequest) private p2pRequests;
 
-    function getRequests(address _borrower)
-        external
-        view
-        returns (Request memory)
-    {
-        return requests[_borrower];
+    struct guranteeRequest {
+        address borrower; // * Address of the borrower
+        address lender; // * Address of the Lender
+        uint256 totalAmount; // * Amount looking for the gurantee
+        bool requestAccepted; // * Request Raised by the lender accepted or not
     }
 
-    // * To deploy the Deal Contract
-    function deploy() internal {
-        Request storage requestDetails = request;
+    guranteeRequest private guranteeRequestInstance;
+
+    // * To store all the guranteeRequest made in the protocol
+    mapping(address => guranteeRequest) private guranteeRequests;
+
+    // * FUNCTION: To get the p2pRequests made by a particualr address
+    function getP2PRequests(address _borrower) external view returns (p2pRequest memory) {
+        return p2pRequests[_borrower];
+    }
+
+    // * FUNCTION: To get the p2pRequests made by a particualr address
+    function getGuranteeRequests(address _borrower) external view returns (p2pRequest memory) {
+        return guranteeRequests[_borrower];
+    }
+
+    // * FUNCTION: To deploy the Deal Contract
+    function p2pDeploy() internal {
+        p2pRequest storage requestDetails = p2pRequestInstance;
 
         dealContract = new deal_contract(
             requestDetails.borrower,
@@ -50,24 +64,24 @@ contract deployer_contract is Context {
             requestDetails.noOfInstalments
         );
 
-        requests[requestDetails.borrower].dealAddress = address(dealContract);
+        p2pRequests[requestDetails.borrower].dealAddress = address(dealContract);
 
-        delete request;
+        delete p2pRequest;
 
         // emit Event to notify both lender and borrower
     }
 
-    // * To raise the request to borrow
-    function raiseRequest(
+    // * FUNCTION: To raise the p2pRequest to borrow
+    function p2pRaiseRequest(
         uint256 _instalmentAmount,
         uint256 _totalAmount,
         uint256 _interestRate,
         uint16 _noOfInstalments,
         address _lender
     ) external {
-        require(!requests[_msgSender()].requestAccepted, "ERR:RA"); // RA => Request Accepted
+        require(!p2pRequests[_msgSender()].requestAccepted, "ERR:RA"); // RA => Request Accepted
 
-        Request storage requestDetails = request;
+        p2pRequest storage requestDetails = p2pRequestInstance;
 
         requestDetails.borrower = _msgSender();
         requestDetails.lender = _lender;
@@ -76,19 +90,34 @@ contract deployer_contract is Context {
         requestDetails.interestRate = _interestRate;
         requestDetails.noOfInstalments = _noOfInstalments;
 
-        requests[_msgSender()] = requestDetails;
+        p2pRequests[_msgSender()] = requestDetails;
 
         // emit event to notify lender
     }
 
-    // * To accept the request made by the borrower
-    function acceptRequest(address _borrower) external payable {
-        require(!requests[_borrower].requestAccepted, "ERR:AA"); // AA =>Already Accepted
+    // * FUNCTION: To raise the request for backing the loan from the protocol
+    function guaranteeRaiseRequest(uint256 _totalAmount, address _lender) external {
+        require(!guranteeRequests[_msgSender()].requestAccepted, "ERR:RA"); // RA => Request Accepted
+
+        guranteeRequest storage requestDetails = guranteeRequestInstance;
+
+        requestDetails.borrower = _msgSender();
+        requestDetails.lender = _lender;
+        requestDetails.totalAmount = _totalAmount;
+
+        guranteeRequests[_msgSender()] = requestDetails;
+
+        // emit event to notify lender
+    }
+
+    // * FUNCTION: To accept the p2pRequest made by the borrower
+    function p2pAcceptRequest(address _borrower) external payable {
+        require(!p2pRequests[_borrower].requestAccepted, "ERR:AA"); // AA =>Already Accepted
 
         uint256 value = msg.value;
-        require(requests[_borrower].totalAmount == value, "ERR:WV"); // WV => Wrong Value
+        require(p2pRequests[_borrower].totalAmount == value, "ERR:WV"); // WV => Wrong Value
 
-        requests[_borrower].requestAccepted = true;
+        p2pRequests[_borrower].requestAccepted = true;
 
         deploy();
 
@@ -97,4 +126,14 @@ contract deployer_contract is Context {
 
         // emit event to notify borrower
     }
+
+    // * FUNCTION: To accept the guranteeRequest made by the borrower
+    function guranteeAcceptRequest (address _borrower) external {
+        require(!guranteeRequests[_borrower].requestAccepted, "ERR:AA"); // AA =>Already Accepted
+
+        p2pRequests[_borrower].requestAccepted = true;
+
+        // emit event to notify borrower
+    }
+
 }
