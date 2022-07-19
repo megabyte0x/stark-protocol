59854db9f0e419ac242197f4c58141098cbd97f1
Yash Garg <yashygkf@gmail.com>
2022-07-20 01:13:51 +0530

    add: seperate Interface folder created, and Interface file for Stark.sol added

---
+++ backend/contracts/interfaces/IStark.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+interface Istark_protocol {
+    function getLockedBalance(address tokenAddress, address userAddress)
+        external
+        view
+        returns (uint256);
+
+    function requestChange_LockBalance(
+        address _tokenAddress,
+        address _lender,
+        address _borrower,
+        uint256 _tokenAmount
+    ) external;
+}
