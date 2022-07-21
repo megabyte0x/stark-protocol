4b45cf67589eb96506063b36ded06c340e04c544
Harendra Shakya <105966146+harendra-shakya@users.noreply.github.com>
2022-07-21 21:32:01 +0530

    changed name and updated sbt file

--- backend/contracts/CreditScore.sol
+++ backend/contracts/CreditScore.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@tableland/evm/contracts/ITablelandTables.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
+import "base64-sol/base64.sol";
 
-
-contract sbt_table is Context, ERC721URIStorage, Ownable  {
+contract CreditScore is Context, ERC721URIStorage, Ownable  {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -23,16 +24,24 @@ contract sbt_table is Context, ERC721URIStorage, Ownable  {
     // Our will be pulled from the network
     string private _baseURIString = "https://testnet.tableland.network/query?s=";
 
-    string private constant RED_SVG='<svg id="eChK4yXtexE1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" width="300" height="300" style="background-color:#FF0000"><text dx="0" dy="0" font-family="&quot;Roboto&quot;" font-size="15" font-weight="400" transform="matrix(4.917124 0 0 5.062497 87.876901 175.927052)" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0"><![CDATA[100]]></tspan></text></svg>';
+    string private constant STARTING_SVG='<svg id="eChK4yXtexE1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" width="450" height="300" style="background-color:';
+    // in b/w color credit score will come
+    // red -> #FF0000
+    // yellow -> #fdff00
+    // green -> #00FF00
+    string private constant MIDDLE_SVG='"><text dx="0" dy="0" font-family="&quot;Roboto&quot;" font-size="15" font-weight="400" transform="matrix(4.917124 0 0 5.062497 87.876901 175.927052)" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">';
+    // in b/w credit score will come
+    string private constant ENDING_SVG='</tspan></text></svg>';
 
-    string private constant YELLOW_SVG='<svg id="eChK4yXtexE1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" width="300" height="300" style="background-color:#fdff00"><text dx="0" dy="0" font-family="&quot;Roboto&quot;" font-size="15" font-weight="400" transform="matrix(4.917124 0 0 5.062497 87.876901 175.927052)" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0"><![CDATA[100]]></tspan></text></svg>';
-
-    string private constant GREEN_SVG='<svg id="eChK4yXtexE1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" width="300" height="300" style="background-color:#00FF00"><text dx="0" dy="0" font-family="&quot;Roboto&quot;" font-size="15" font-weight="400" transform="matrix(4.917124 0 0 5.062497 87.876901 175.927052)" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0"><![CDATA[100]]></tspan></text></svg>>';
-
+    modifier notMinted() {
+        address owner = this.ownerOf(_tokenIds.current());
+        require(owner != msg.sender, "Already minted!");
+        _;
+    }
 
     // Called only when the smart contract is created
     // registry = 0x4b48841d4b32C4650E4ABc117A03FE8B51f38F68
-    constructor(address registry) ERC721("Pixel", "ITM") {
+    constructor(address registry) ERC721("Stark Credit Score", "SCS") {
         /*
          * The Tableland address on your current chain
          */
@@ -70,7 +79,7 @@ contract sbt_table is Context, ERC721URIStorage, Ownable  {
      * Any time a token is minted, a new row of metadata will be
      * dynamically inserted into the metadata table.
      */
-    function safeMint(address to) public returns (uint256) {
+    function safeMint(address to) public notMinted returns (uint256) {
         uint256 newItemId = _tokenIds.current();
         _tableland.runSQL(
             address(this),
@@ -84,23 +93,36 @@ contract sbt_table is Context, ERC721URIStorage, Ownable  {
                 ", 'not.implemented.xyz', 100)"
             )
         );
-        _safeMint(to, newItemId, "");
+
+        string memory color = "#fdff00";
+        string memory score = 100;
+
+        if(score > 200) {
+            color = "#00FF00"; // green
+        } else if (score < 50) {
+            color = "#FF0000"; // red
+        }
+
+        string memory _image = string.concat(STARTING_SVG, color, MIDDLE_SVG, Strings.toString(score), ENDING_SVG);
+
+        string memory finalTokenUri = string.concat(
+            '{"name": "Credit Score", "description": "A Credit Score SBT provided by Stark", "image": "data:image/svg+xml;base64,',
+            Base64.encode(bytes(_image)),
+            '"}'
+        );
+
+        _safeMint(to, newItemId);
+        _setTokenURI(newItemId, finalTokenUri);
         _tokenIds.increment();
         return newItemId;
     }
 
-    /*
-     * @dev makeMove is an example of how to encode gameplay into both the
-     * smart contract and the metadata. Whenever a token owner calls
-     * make move, they can supply a new x,y coordinate and update
-     * their token metadata.
-     */
     function changeCredit(
         uint256 tokenId,
         uint256 _score
     ) public {
         // Check token ownership
-        require(this.ownerOf(tokenId) == msg.sender, "Invalid owner");
+        require(this.ownerOf(tokenId) == deployer, "Invalid owner");
         // Simple on-chain gameplay enforcement
         // Update the row in tableland
         _tableland.runSQL(
@@ -122,17 +144,13 @@ contract sbt_table is Context, ERC721URIStorage, Ownable  {
         return _baseURIString;
     }
 
-    /*
-     * @dev tokenURI is an example of how to turn a row in your table back into
-     * erc721 compliant metadata JSON. Here, we do a simple SELECT statement
-     * with function that converts the result into json.
-     */
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
-        string memory base = _baseURI();
-
-        /* We will give token viewers a way to get at our table metadata */
-        return;
+    function _beforeTokenTransfer(
+        address from, 
+        address to, 
+        uint256 tokenId
+    ) internal override virtual {
+        require(from == address(0), "Err: token transfer is BLOCKED");   
+        super._beforeTokenTransfer(from, to, tokenId);  
     }
 
     function metadataURI() public view returns (string memory) {
