f0e62c8ef58fb8f711743f6a09c8f3155a4f857d
Yash Garg <yashygkf@gmail.com>
2022-07-23 13:34:52 +0530

    delete: p2p functionality

--- backend/contracts/CreditLogic.sol
+++ backend/contracts/CreditLogic.sol
@@ -17,17 +17,17 @@ contract CreditLogic is Context, Ownable {
         starkProtocolAddress = _starkProtocolAddress;
     }
 
-    struct P2PRequest {
-        address borrower; // * Address of the borrower
-        address lender; // * Address of the Lender
-        address dealAddress; // * Address of the Deal Contract
-        address tokenAddress;
-        uint256 instalmentAmount; //* Amount to be paid in each instalment
-        uint256 totalAmount; // * Total Amount borrowed
-        uint256 interestRate; // * Interest Rate by the Lender
-        uint16 noOfInstalments; // * No of Instalments
-        bool requestAccepted; // * Request Raised by the lender accepted or not
-    }
+    // struct P2PRequest {
+    //     address borrower; // * Address of the borrower
+    //     address lender; // * Address of the Lender
+    //     address dealAddress; // * Address of the Deal Contract
+    //     address tokenAddress;
+    //     uint256 instalmentAmount; //* Amount to be paid in each instalment
+    //     uint256 totalAmount; // * Total Amount borrowed
+    //     uint256 interestRate; // * Interest Rate by the Lender
+    //     uint16 noOfInstalments; // * No of Instalments
+    //     bool requestAccepted; // * Request Raised by the lender accepted or not
+    // }
 
     struct GuarantyRequest {
         address borrower; // * Address of the borrower
@@ -43,88 +43,88 @@ contract CreditLogic is Context, Ownable {
     // guarantyRequests[_lender][_borrower]
     mapping(address => mapping(address => GuarantyRequest)) private guarantyRequests;
 
-    // * To store all the p2pRequests made in the protocol
-    // lender & borrower -> request
-    mapping(address => mapping(address => P2PRequest)) private p2pRequests;
+    // // * To store all the p2pRequests made in the protocol
+    // // lender & borrower -> request
+    // mapping(address => mapping(address => P2PRequest)) private p2pRequests;
 
     ///////////////////////
     //// p2p functions ///
     //////////////////////
 
-    // * FUNCTION: To deploy the Deal Contract
-    function p2pDeploy(address _lender, address _borrower) internal {
-        P2PRequest memory requestDetails = p2pRequests[_lender][_borrower];
+    // // * FUNCTION: To deploy the Deal Contract
+    // function p2pDeploy(address _lender, address _borrower) internal {
+    //     P2PRequest memory requestDetails = p2pRequests[_lender][_borrower];
 
-        dealContract = new deal_contract(
-            requestDetails.borrower,
-            requestDetails.lender,
-            starkProtocolAddress,
-            requestDetails.tokenAddress,
-            requestDetails.instalmentAmount,
-            requestDetails.totalAmount,
-            requestDetails.interestRate,
-            requestDetails.noOfInstalments
-        );
+    //     dealContract = new deal_contract(
+    //         requestDetails.borrower,
+    //         requestDetails.lender,
+    //         starkProtocolAddress,
+    //         requestDetails.tokenAddress,
+    //         requestDetails.instalmentAmount,
+    //         requestDetails.totalAmount,
+    //         requestDetails.interestRate,
+    //         requestDetails.noOfInstalments
+    //     );
 
-        p2pRequests[requestDetails.lender][requestDetails.borrower].dealAddress = address(
-            dealContract
-        );
+    //     p2pRequests[requestDetails.lender][requestDetails.borrower].dealAddress = address(
+    //         dealContract
+    //     );
 
-        starkContract.addAllowContracts(address(dealContract));
+    //     starkContract.addAllowContracts(address(dealContract));
 
-        // emit Event to notify both lender and borrower
-    }
+    //     // emit Event to notify both lender and borrower
+    // }
 
-    // * FUNCTION: To raise the P2PRequest to borrow
-    function p2pRaiseRequest(
-        uint256 _instalmentAmount,
-        uint256 _totalAmount,
-        uint256 _interestRate,
-        uint16 _noOfInstalments,
-        address _lender,
-        address _tokenAddress
-    ) external {
-        require(!p2pRequests[_lender][_msgSender()].requestAccepted, "ERR:RA"); // RA => Request Accepted
+    // // * FUNCTION: To raise the P2PRequest to borrow
+    // function p2pRaiseRequest(
+    //     uint256 _instalmentAmount,
+    //     uint256 _totalAmount,
+    //     uint256 _interestRate,
+    //     uint16 _noOfInstalments,
+    //     address _lender,
+    //     address _tokenAddress
+    // ) external {
+    //     require(!p2pRequests[_lender][_msgSender()].requestAccepted, "ERR:RA"); // RA => Request Accepted
 
-        P2PRequest memory requestDetails;
+    //     P2PRequest memory requestDetails;
 
-        requestDetails.borrower = _msgSender();
-        requestDetails.lender = _lender;
-        requestDetails.instalmentAmount = _instalmentAmount;
-        requestDetails.totalAmount = _totalAmount;
-        requestDetails.interestRate = _interestRate;
-        requestDetails.noOfInstalments = _noOfInstalments;
-        requestDetails.tokenAddress = _tokenAddress;
+    //     requestDetails.borrower = _msgSender();
+    //     requestDetails.lender = _lender;
+    //     requestDetails.instalmentAmount = _instalmentAmount;
+    //     requestDetails.totalAmount = _totalAmount;
+    //     requestDetails.interestRate = _interestRate;
+    //     requestDetails.noOfInstalments = _noOfInstalments;
+    //     requestDetails.tokenAddress = _tokenAddress;
 
-        p2pRequests[_lender][_msgSender()] = requestDetails;
+    //     p2pRequests[_lender][_msgSender()] = requestDetails;
 
-        // emit event to notify lender
-    }
+    //     // emit event to notify lender
+    // }
 
-    // * FUNCTION: To accept the P2PRequest made by the borrower
-    function p2pAcceptRequest(address _borrower) external payable {
-        P2PRequest memory requestDetails = p2pRequests[_msgSender()][_borrower];
+    // // * FUNCTION: To accept the P2PRequest made by the borrower
+    // function p2pAcceptRequest(address _borrower) external payable {
+    //     P2PRequest memory requestDetails = p2pRequests[_msgSender()][_borrower];
 
-        require(!requestDetails.requestAccepted, "ERR:AA"); // AA =>Already Accepted
-        uint256 tokenAmountinProtocol = starkContract.getSupplyBalance(
-            requestDetails.tokenAddress,
-            _msgSender()
-        );
-        require(requestDetails.totalAmount <= tokenAmountinProtocol, "ERR:NE"); // NA => Not Enough Amount
+    //     require(!requestDetails.requestAccepted, "ERR:AA"); // AA =>Already Accepted
+    //     uint256 tokenAmountinProtocol = starkContract.getSupplyBalance(
+    //         requestDetails.tokenAddress,
+    //         _msgSender()
+    //     );
+    //     require(requestDetails.totalAmount <= tokenAmountinProtocol, "ERR:NE"); // NA => Not Enough Amount
 
-        starkContract.lockBalanceChanges(
-            requestDetails.tokenAddress,
-            _msgSender(),
-            _borrower,
-            requestDetails.totalAmount
-        );
+    //     starkContract.lockBalanceChanges(
+    //         requestDetails.tokenAddress,
+    //         _msgSender(),
+    //         _borrower,
+    //         requestDetails.totalAmount
+    //     );
 
-        p2pRequests[_msgSender()][_borrower].requestAccepted = true;
+    //     p2pRequests[_msgSender()][_borrower].requestAccepted = true;
 
-        p2pDeploy(_msgSender(), _borrower);
+    //     p2pDeploy(_msgSender(), _borrower);
 
-        // emit event to notify borrower
-    }
+    //     // emit event to notify borrower
+    // }
 
     ////////////////////////////
     ///// guaranty functions ///
@@ -179,21 +179,21 @@ contract CreditLogic is Context, Ownable {
     ///// getter functions ///
     /////////////////////////
 
-    // * FUNCTION: To get the p2pRequests made by a particualr address
-    function getP2PRequest(address _lender, address _borrower)
-        external
-        view
-        returns (P2PRequest memory)
-    {
-        return p2pRequests[_lender][_borrower];
-    }
+    // // * FUNCTION: To get the p2pRequests made by a particualr address
+    // function getP2PRequest(address _lender, address _borrower)
+    //     external
+    //     view
+    //     returns (P2PRequest memory)
+    // {
+    //     return p2pRequests[_lender][_borrower];
+    // }
 
-    // * FUNCTION: To get the p2pRequests made by a particualr address
-    function getGuarantyRequest(address _lender, address _borrower)
-        external
-        view
-        returns (GuarantyRequest memory)
-    {
-        return guarantyRequests[_lender][_borrower];
-    }
+    // // * FUNCTION: To get the p2pRequests made by a particualr address
+    // function getGuarantyRequest(address _lender, address _borrower)
+    //     external
+    //     view
+    //     returns (GuarantyRequest memory)
+    // {
+    //     return guarantyRequests[_lender][_borrower];
+    // }
 }
--- backend/contracts/p2p/Deal.sol
+++ backend/contracts/p2p/Deal.sol
@@ -1,219 +1,219 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.15;
+// //SPDX-License-Identifier: Unlicense
+// pragma solidity 0.8.15;
 
-import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "../interfaces/IStark.sol";
+// import "@openzeppelin/contracts/utils/Context.sol";
+// import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+// import "../interfaces/IStark.sol";
 
-contract deal_contract is Context {
-    using SafeMath for uint256;
+// contract deal_contract is Context {
+//     using SafeMath for uint256;
 
-    address private deployer;
-    address private borrower;
-    address private lender;
+//     address private deployer;
+//     address private borrower;
+//     address private lender;
 
-    Istark_protocol starkContract;
+//     Istark_protocol starkContract;
 
-    struct DealDetials {
-        address tokenAddress;
-        uint256 totalAmount; // * Total amount borrowed by the borrower
-        uint256 totalAmountToPay; // * Total amount including interest left to be paid
-        uint256 amountPaidTotal; // * Amount paid by the borrower in total
-        uint256 instalmentAmt; // * Amount to be paid per insalment
-        uint256 timeRentedSince; // * Time when the deal started
-        uint256 interestRate; // * Interest rate decided by the lender.
-        uint256 addedInterestRate; // * Additional Interest Rate for additional no. of instalments.
-        uint16 noOfInstalments; // * No of instalments in which borrower will pay amount
-        bool addedInstalments; // * If borrower got more instalments after request.
-    }
+//     struct DealDetials {
+//         address tokenAddress;
+//         uint256 totalAmount; // * Total amount borrowed by the borrower
+//         uint256 totalAmountToPay; // * Total amount including interest left to be paid
+//         uint256 amountPaidTotal; // * Amount paid by the borrower in total
+//         uint256 instalmentAmt; // * Amount to be paid per insalment
+//         uint256 timeRentedSince; // * Time when the deal started
+//         uint256 interestRate; // * Interest rate decided by the lender.
+//         uint256 addedInterestRate; // * Additional Interest Rate for additional no. of instalments.
+//         uint16 noOfInstalments; // * No of instalments in which borrower will pay amount
+//         bool addedInstalments; // * If borrower got more instalments after request.
+//     }
 
-    DealDetials private deal;
+//     DealDetials private deal;
 
-    struct AdditionalRequest {
-        uint16 noOfInstalments; // * No of additional instalments
-        uint256 interestRate; // * Interest Rate
-        bool isAccepted; // * Request Accepted or Not
-    }
+//     struct AdditionalRequest {
+//         uint16 noOfInstalments; // * No of additional instalments
+//         uint256 interestRate; // * Interest Rate
+//         bool isAccepted; // * Request Accepted or Not
+//     }
 
-    mapping(address => AdditionalRequest) additionRequest;
+//     mapping(address => AdditionalRequest) additionRequest;
 
-    constructor(
-        address _borrower,
-        address _lender,
-        address _starkAddress,
-        address _tokenAddress,
-        uint256 _instalmentAmount,
-        uint256 _totalAmount,
-        uint256 _interestRate,
-        uint16 _noOfInstalments
-    ) {
-        deployer = _msgSender();
-        borrower = _borrower;
-        lender = _lender;
-        starkContract = Istark_protocol(_starkAddress);
+//     constructor(
+//         address _borrower,
+//         address _lender,
+//         address _starkAddress,
+//         address _tokenAddress,
+//         uint256 _instalmentAmount,
+//         uint256 _totalAmount,
+//         uint256 _interestRate,
+//         uint16 _noOfInstalments
+//     ) {
+//         deployer = _msgSender();
+//         borrower = _borrower;
+//         lender = _lender;
+//         starkContract = Istark_protocol(_starkAddress);
 
-        DealDetials storage dealDetails = deal;
+//         DealDetials storage dealDetails = deal;
 
-        dealDetails.noOfInstalments = _noOfInstalments;
-        dealDetails.totalAmount = _totalAmount;
-        dealDetails.interestRate = _interestRate;
-        dealDetails.timeRentedSince = uint256(block.timestamp);
-        dealDetails.instalmentAmt = getInstalmentAmount(_instalmentAmount);
-        dealDetails.totalAmountToPay = _totalAmount + dealDetails.instalmentAmt;
-        dealDetails.tokenAddress = _tokenAddress;
-    }
+//         dealDetails.noOfInstalments = _noOfInstalments;
+//         dealDetails.totalAmount = _totalAmount;
+//         dealDetails.interestRate = _interestRate;
+//         dealDetails.timeRentedSince = uint256(block.timestamp);
+//         dealDetails.instalmentAmt = getInstalmentAmount(_instalmentAmount);
+//         dealDetails.totalAmountToPay = _totalAmount + dealDetails.instalmentAmt;
+//         dealDetails.tokenAddress = _tokenAddress;
+//     }
 
-    modifier onlyBorrower() {
-        require(msg.sender == borrower, "ERR:BO"); // BO => Borrower only
-        _;
-    }
+//     modifier onlyBorrower() {
+//         require(msg.sender == borrower, "ERR:BO"); // BO => Borrower only
+//         _;
+//     }
 
-    modifier onlyLender() {
-        require(msg.sender == lender, "ERR:LO"); // BL => Lender only
-        _;
-    }
+//     modifier onlyLender() {
+//         require(msg.sender == lender, "ERR:LO"); // BL => Lender only
+//         _;
+//     }
 
-    // * FUNCTION: To get the address of the borrower.
-    function getBorrower() public view returns (address) {
-        return borrower;
-    }
+//     // * FUNCTION: To get the address of the borrower.
+//     function getBorrower() public view returns (address) {
+//         return borrower;
+//     }
 
-    // * FUNCTION: To get the address of the lender.
-    function getLender() public view returns (address) {
-        return lender;
-    }
+//     // * FUNCTION: To get the address of the lender.
+//     function getLender() public view returns (address) {
+//         return lender;
+//     }
 
-    // * FUNCTION: To get the detials of the Deal.
-    function getDealDetails() public view returns (DealDetials memory) {
-        return deal;
-    }
+//     // * FUNCTION: To get the detials of the Deal.
+//     function getDealDetails() public view returns (DealDetials memory) {
+//         return deal;
+//     }
 
-    // * FUNCTION: To get the Instalment Amount
-    function getInstalmentAmount(uint256 _instalmentAmount) public view returns (uint256) {
-        DealDetials memory dealDetails = deal;
-        uint256 interestAmount = (_instalmentAmount * dealDetails.interestRate).div(
-            uint256(dealDetails.noOfInstalments  * 100)
-        );
+//     // * FUNCTION: To get the Instalment Amount
+//     function getInstalmentAmount(uint256 _instalmentAmount) public view returns (uint256) {
+//         DealDetials memory dealDetails = deal;
+//         uint256 interestAmount = (_instalmentAmount * dealDetails.interestRate).div(
+//             uint256(dealDetails.noOfInstalments  * 100)
+//         );
 
-        uint256 instalmentAmount = _instalmentAmount + interestAmount;
-        return instalmentAmount;
-    }
+//         uint256 instalmentAmount = _instalmentAmount + interestAmount;
+//         return instalmentAmount;
+//     }
 
-    // * FUNCTION: To get the number of instalments
-    function getNoOfInstalments() public view returns (uint16) {
-        return deal.noOfInstalments;
-    }
+//     // * FUNCTION: To get the number of instalments
+//     function getNoOfInstalments() public view returns (uint16) {
+//         return deal.noOfInstalments;
+//     }
 
-    // * FUNCTION: To get the total amount owed
-    function getTotalAmountOwed() public view returns (uint256) {
-        return deal.totalAmount;
-    }
+//     // * FUNCTION: To get the total amount owed
+//     function getTotalAmountOwed() public view returns (uint256) {
+//         return deal.totalAmount;
+//     }
 
-    // * FUNCTION: To get the amount left to be paid
-    function getTotalAmountLeft() public view returns (uint256) {
-        return deal.totalAmountToPay;
-    }
+//     // * FUNCTION: To get the amount left to be paid
+//     function getTotalAmountLeft() public view returns (uint256) {
+//         return deal.totalAmountToPay;
+//     }
 
-    // * FUNCTION: To get the interest rate
-    function getInterestRate() public view returns (uint256) {
-        return deal.interestRate;
-    }
+//     // * FUNCTION: To get the interest rate
+//     function getInterestRate() public view returns (uint256) {
+//         return deal.interestRate;
+//     }
 
-    // * FUNCTION: Pay the amount left at once
-    function payAtOnce() external onlyBorrower {
-        DealDetials memory dealDetails = deal;
-        require(dealDetails.noOfInstalments > 0, "ERR:NM"); // NM => No more installments
-        require(dealDetails.amountPaidTotal < dealDetails.totalAmount, "ERR:NM"); // NM => No more installments
+//     // * FUNCTION: Pay the amount left at once
+//     function payAtOnce() external onlyBorrower {
+//         DealDetials memory dealDetails = deal;
+//         require(dealDetails.noOfInstalments > 0, "ERR:NM"); // NM => No more installments
+//         require(dealDetails.amountPaidTotal < dealDetails.totalAmount, "ERR:NM"); // NM => No more installments
 
-        // uint256 value = msg.value;
-        uint256 amountLeftToPay = getTotalAmountLeft();
-        // require(value == amountLeftToPay, "ERR:WV"); // WV => Wrong value
+//         // uint256 value = msg.value;
+//         uint256 amountLeftToPay = getTotalAmountLeft();
+//         // require(value == amountLeftToPay, "ERR:WV"); // WV => Wrong value
 
-        starkContract.repayChanges(dealDetails.tokenAddress, lender, borrower, amountLeftToPay);
+//         starkContract.repayChanges(dealDetails.tokenAddress, lender, borrower, amountLeftToPay);
 
-        deal.amountPaidTotal += amountLeftToPay;
-        deal.totalAmountToPay -= amountLeftToPay;
-    }
+//         deal.amountPaidTotal += amountLeftToPay;
+//         deal.totalAmountToPay -= amountLeftToPay;
+//     }
 
-    // * FUNCTION: Pay the pre-defined amount in instalments not necessarily periodically.
-    function payInInstalment() external payable onlyBorrower {
-        DealDetials memory dealDetails = deal;
+//     // * FUNCTION: Pay the pre-defined amount in instalments not necessarily periodically.
+//     function payInInstalment() external payable onlyBorrower {
+//         DealDetials memory dealDetails = deal;
 
-        require(dealDetails.noOfInstalments > 0, "ERR:NM"); // NM => No more installments
-        require(dealDetails.amountPaidTotal < dealDetails.totalAmount, "ERR:NM"); // NM => No more installments
+//         require(dealDetails.noOfInstalments > 0, "ERR:NM"); // NM => No more installments
+//         require(dealDetails.amountPaidTotal < dealDetails.totalAmount, "ERR:NM"); // NM => No more installments
 
-        // * amtToLenderOnly: Amount with standard interest
-        uint256 amtToLenderOnly = dealDetails.instalmentAmt;
+//         // * amtToLenderOnly: Amount with standard interest
+//         uint256 amtToLenderOnly = dealDetails.instalmentAmt;
 
-        if (dealDetails.addedInstalments) {
-            // * totalInterestedAmount: Amount after additional interest is added
-            uint256 totalInterestedAmount = amtToLenderOnly +
-                (dealDetails.addedInterestRate * dealDetails.instalmentAmt);
+//         if (dealDetails.addedInstalments) {
+//             // * totalInterestedAmount: Amount after additional interest is added
+//             uint256 totalInterestedAmount = amtToLenderOnly +
+//                 (dealDetails.addedInterestRate * dealDetails.instalmentAmt);
 
-            // require(value == totalInterestedAmount, "ERR:WV"); // WV => Wrong value
+//             // require(value == totalInterestedAmount, "ERR:WV"); // WV => Wrong value
 
-            // * amtToLender: Amount after with 95% of additional interest is added
-            uint256 amtToLender = amtToLenderOnly +
-                (dealDetails.instalmentAmt * dealDetails.addedInterestRate * 95 * 10**16);
+//             // * amtToLender: Amount after with 95% of additional interest is added
+//             uint256 amtToLender = amtToLenderOnly +
+//                 (dealDetails.instalmentAmt * dealDetails.addedInterestRate * 95 * 10**16);
 
-            // * amtToProtocol: Amount after with 5% of additional interest is added
-            uint256 amtToProtocol = dealDetails.instalmentAmt *
-                dealDetails.addedInterestRate *
-                5 *
-                10**16;
+//             // * amtToProtocol: Amount after with 5% of additional interest is added
+//             uint256 amtToProtocol = dealDetails.instalmentAmt *
+//                 dealDetails.addedInterestRate *
+//                 5 *
+//                 10**16;
 
-            // (bool successInLender, ) = lender.call{value: amtToLender}("");
-            // require(successInLender, "ERR:OT"); //OT => On Transfer
+//             // (bool successInLender, ) = lender.call{value: amtToLender}("");
+//             // require(successInLender, "ERR:OT"); //OT => On Transfer
 
-            starkContract.repayChanges(dealDetails.tokenAddress, lender, borrower, amtToLender);
+//             starkContract.repayChanges(dealDetails.tokenAddress, lender, borrower, amtToLender);
 
-            // (bool successInBorrower, ) = deployer.call{value: amtToProtocol}("");
-            // require(successInBorrower, "ERR:OT"); //OT => On Transfer
-            deal.amountPaidTotal += amtToLender;
-            deal.totalAmountToPay -= amtToLender;
-            //! TODO: Function to pass the value to the protocol
-        } else {
-            starkContract.repayChanges(
-                dealDetails.tokenAddress,
-                lender,
-                borrower,
-                amtToLenderOnly
-            );
+//             // (bool successInBorrower, ) = deployer.call{value: amtToProtocol}("");
+//             // require(successInBorrower, "ERR:OT"); //OT => On Transfer
+//             deal.amountPaidTotal += amtToLender;
+//             deal.totalAmountToPay -= amtToLender;
+//             //! TODO: Function to pass the value to the protocol
+//         } else {
+//             starkContract.repayChanges(
+//                 dealDetails.tokenAddress,
+//                 lender,
+//                 borrower,
+//                 amtToLenderOnly
+//             );
 
-            deal.amountPaidTotal += amtToLenderOnly;
-            deal.totalAmountToPay -= amtToLenderOnly;
-        }
-        --deal.noOfInstalments;
-    }
+//             deal.amountPaidTotal += amtToLenderOnly;
+//             deal.totalAmountToPay -= amtToLenderOnly;
+//         }
+//         --deal.noOfInstalments;
+//     }
 
-    // * FUNCTION: Request the Lender for more instalments
-    function requestNoOfInstalment(uint16 noOfAddInstalments, uint256 _interestRate)
-        external
-        onlyBorrower
-    {
-        require(noOfAddInstalments >= 3, "ERR:MR"); // MR => Minimum required no of instalments
+//     // * FUNCTION: Request the Lender for more instalments
+//     function requestNoOfInstalment(uint16 noOfAddInstalments, uint256 _interestRate)
+//         external
+//         onlyBorrower
+//     {
+//         require(noOfAddInstalments >= 3, "ERR:MR"); // MR => Minimum required no of instalments
 
-        additionRequest[_msgSender()] = AdditionalRequest(
-            noOfAddInstalments,
-            _interestRate,
-            false
-        );
+//         additionRequest[_msgSender()] = AdditionalRequest(
+//             noOfAddInstalments,
+//             _interestRate,
+//             false
+//         );
 
-        // emit event
-    }
+//         // emit event
+//     }
 
-    // * FUNCTION: Accept the request made the Lender for more instalments
-    function acceptRequestOfInstalment(
-        address _borrower,
-        uint16 _noOfAddInstalments,
-        uint256 _interestRate
-    ) external onlyLender {
-        require(!additionRequest[_borrower].isAccepted, "ERR:AA"); // AA => Already Accepted
+//     // * FUNCTION: Accept the request made the Lender for more instalments
+//     function acceptRequestOfInstalment(
+//         address _borrower,
+//         uint16 _noOfAddInstalments,
+//         uint256 _interestRate
+//     ) external onlyLender {
+//         require(!additionRequest[_borrower].isAccepted, "ERR:AA"); // AA => Already Accepted
 
-        additionRequest[_borrower].isAccepted = true;
+//         additionRequest[_borrower].isAccepted = true;
 
-        deal.noOfInstalments += _noOfAddInstalments;
-        deal.addedInterestRate = _interestRate;
-        deal.addedInstalments = true;
-    }
-}
+//         deal.noOfInstalments += _noOfAddInstalments;
+//         deal.addedInterestRate = _interestRate;
+//         deal.addedInstalments = true;
+//     }
+// }
